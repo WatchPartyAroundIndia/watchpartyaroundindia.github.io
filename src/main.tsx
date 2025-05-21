5aed850be73da423ef90f6b12bf885388253bc0d
Anirudh Jwala <anirudhsai2@gmail.com>
2025-05-21 20:14:01 +0530

    refactor: update router configuration and ensure base path is set

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,23 @@ import App2024 from "./2024/App.tsx";
 
 import "./index.css";
 
-const router = createBrowserRouter([
+const basePath = import.meta.env.BASE_URL || "/";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/2024",
+      element: <App2024 />,
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/2024",
-    element: <App2024 />,
-  },
-]);
+    basename: basePath,
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
