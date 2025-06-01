a681f98236cc832cc7c7a386a959dc042579d0e9
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-01 09:22:19 +0530

    feat(Sponsors): add venue and other sponsors sections for improved organization

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -20,6 +20,20 @@ const Sponsors: FC<SponsorsProps> = () => {
           Meet the sponsors supporting our mission
         </h3>
         <div className="h-12" />
+        <div className="flex flex-col max-w-6xl mx-auto">
+          <h2 className="text-base md:text-xl lg:text-2xl text-center mb-3 font-calSans">
+            Venue Sponsor
+          </h2>
+          <div className="flex flex-wrap justify-center items-center gap-8 lg:gap-x-14 lg:gap-y-12">
+            <div className="flex justify-center items-center gap-2">
+              <p className="font-calSans text-base lg:text-lg">Tasks</p>
+            </div>
+          </div>
+          <div className="h-8" />
+          <h2 className="text-base md:text-xl lg:text-2xl text-center mb-3 font-calSans">
+            Other Sponsors
+          </h2>
+        </div>
       </div>
     </div>
   );
