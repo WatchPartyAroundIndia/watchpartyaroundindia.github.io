b0b72e36c318acd666699b7fc57131d26981a014
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-01 08:26:47 +0530

    fix(Navbar): adjust button placement and text size for improved layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,19 +7,19 @@ const Navbar: FC<NavbarProps> = () => {
   return (
     <div className="flex items-center w-full px-8 py-10 font-prompt">
       <img src={wwdcLogo} alt="WWDC 2025" width={148} height={54} />
-      <ul className="flex items-center justify-end flex-1 space-x-8">
+      <ul className="items-center justify-end flex-1 space-x-8 hidden md:flex mr-8">
         {["About", "Sponsors", "Speakers", "2024"].map((item, index) => (
           <li
             key={index}
-            className="hover:underline hover:decoration-[#FFC9F0] hover:underline-offset-1 hover:[text-decoration-thickness:5px] cursor-pointer text-2xl"
+            className="hover:underline hover:decoration-[#FFC9F0] hover:underline-offset-1 hover:[text-decoration-thickness:5px] cursor-pointer text-xl lg:text-2xl"
           >
             {item}
           </li>
         ))}
-        <button className="text-2xl text-white bg-[#4884FF] font-medium px-8 py-2 border-2 border-[#0B2131]">
-          Register
-        </button>
       </ul>
+      <button className="ml-auto md:ml-0 text-2xl text-white bg-[#4884FF] font-medium px-8 py-2 border-2 border-[#0B2131]">
+        Register
+      </button>
     </div>
   );
 };
