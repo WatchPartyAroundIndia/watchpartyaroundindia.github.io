d9f862b4ccd9dea8dd7901890f1798b2cf3eb967
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-01 09:12:54 +0530

    fix(Sponsors): update image size and text sizes for improved layout and readability

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -10,13 +10,13 @@ const Sponsors: FC<SponsorsProps> = () => {
         <img
           src={sponsorsRound}
           alt="Sponsors"
-          className="absolute w-40 h-16 lg:w-[350px] lg:h-[120px] right-[-14px] top-[-10px] lg:right-[-65px] lg:top-[-20px]"
+          className="absolute w-44 h-16 lg:w-[350px] lg:h-[120px] right-0 top-[-10px] lg:right-[-65px] lg:top-[-20px]"
         />
-        <h2 className="text-3xl lg:text-6xl font-bold">
+        <h2 className="text-4xl lg:text-6xl font-bold">
           Powered By Our Sponsors
         </h2>
         <div className="h-10" />
-        <h3 className="text-lg lg:text-4xl font-calSans">
+        <h3 className="text-2xl lg:text-4xl font-calSans">
           Meet the sponsors supporting our mission
         </h3>
         <div className="h-12" />
