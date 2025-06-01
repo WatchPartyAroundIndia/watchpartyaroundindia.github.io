5e6cd5b5cd7358da5090f402185be046374a4506
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-01 09:08:04 +0530

    fix(Navbar): update image attributes and button text size for improved layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,11 @@ interface NavbarProps {}
 const Navbar: FC<NavbarProps> = () => {
   return (
     <div className="flex items-center w-full px-8 py-10 font-prompt">
-      <img src={wwdcLogo} alt="WWDC 2025" width={148} height={54} />
+      <img
+        src={wwdcLogo}
+        alt="WWDC 2025"
+        className="w-24 h-9 md:w-32 md:h-11 lg:w-[148px] lg:h-[54px]"
+      />
       <ul className="items-center justify-end flex-1 space-x-8 hidden md:flex mr-8">
         {["About", "Sponsors", "Speakers", "2024"].map((item, index) => (
           <li
@@ -17,7 +21,7 @@ const Navbar: FC<NavbarProps> = () => {
           </li>
         ))}
       </ul>
-      <button className="ml-auto md:ml-0 text-2xl text-white bg-[#4884FF] font-medium px-8 py-2 border-2 border-[#0B2131]">
+      <button className="ml-auto md:ml-0 text-lg md:text-xl lg:text-2xl text-white bg-[#4884FF] font-medium px-4 md:px-6 lg:px-8 py-1 md:py-1.5 lg:py-2 border-2 border-[#0B2131]">
         Register
       </button>
     </div>
