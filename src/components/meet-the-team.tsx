1d14db61fdd26ff9b0a80f18162d2a5a989e71df
Ratnesh Jain <117887125+ratnesh-jain@users.noreply.github.com>
2026-05-27 15:00:04 +0530

    fix: missing team members and favicon fix (#28)
    * Fix missing swift ahmedabad member
    
    * Add missing members
    
    * Remove apple favicon icon

--- src/components/meet-the-team.tsx
+++ src/components/meet-the-team.tsx
@@ -1,64 +1,112 @@
+import adityaBalaji from "../assets/2025/team/Adithya Balaji.jpeg";
+import akankshaSharma from "../assets/2026/team/AkankshaS.jpeg";
+import akshayNjarangal from "../assets/2025/team/Akshay Njarangal.jpeg";
 import akshayPakanati from "../assets/2025/team/Akshay Pakanati.png";
+import amitSamant from "../assets/2025/team/Amit Samant.png";
 import anirudhJwala from "../assets/2025/team/Anirudh Jwala.png";
+import ayushSingh from "../assets/2025/team/Ayush Singh.png";
+import bhavinVaghela from "../assets/2025/team/Bhavin Vaghela.jpg";
 import bhumikaPatel from "../assets/2025/team/Bhumika Patel.jpeg";
 import devanshi from "../assets/2025/team/Devanshi.png";
+import drNikhilMangrulkar from "../assets/2026/team/Dr. Nikhil.jpeg";
 import gokulB from "../assets/2025/team/Gokul B.jpeg";
+import kajalSheth from "../assets/2025/team/Kajal Sheth.png";
+import krishnaPatidar from "../assets/2026/team/krishna-patidar.jpeg";
+import krishnaPermi from "../assets/2026/team/KrishnaPermi.png";
 import maninderSingh from "../assets/2025/team/Maninder Singh.jpeg";
 import nayyanMujadiya from "../assets/2025/team/Nayyan Mujadiya.jpeg";
-import nishantDesai from "../assets/2025/team/Nishant Desai.png";
-import raghavSethi from "../assets/2025/team/Raghav Sethi.png";
-import akshayNjarangal from "../assets/2025/team/Akshay Njarangal.jpeg";
-import shamilRoshan from "../assets/2025/team/Shamil Roshan.jpeg";
-import akankshaSharma from "../assets/2026/team/AkankshaS.jpeg";
 import nikhilNigade from "../assets/2026/team/NikhilN.jpg";
+import nishantDesai from "../assets/2025/team/Nishant Desai.png";
+import pinakRokde from "../assets/2026/team/PinakR.png";
+import raghavSethi from "../assets/2025/team/Raghav Sethi.png";
+import rajRaval from "../assets/2025/team/Raj Raval.png";
+import rangaReddy from "../assets/2025/team/Ranga Reddy.png";
+import ratneshJain from "../assets/2026/team/RatneshJ.jpeg";
 import rimeshJotaniya from "../assets/2026/team/RimeshJ.jpeg";
+import rizwanAhmed from "../assets/2026/team/RizwanA.jpeg";
+import rushitRakhasiya from "../assets/2025/team/Rushit Rakhasiya.png";
+import shamilRoshan from "../assets/2025/team/Shamil Roshan.jpeg";
 import vedangKulkarni from "../assets/2026/team/VedangK.png";
 import yogeshSingh from "../assets/2026/team/YogeshS.jpg";
-import ratneshJain from "../assets/2026/team/RatneshJ.jpeg";
 
 const MeetTheTeam = () => {
   const teamMembers = [
     {
-      id: 23,
+      id: 1,
+      socialLink: "https://www.linkedin.com/in/fuglyab",
+      name: "Adithya Balaji",
+      memojiLogo: adityaBalaji,
+      backgroundColour: "#FFB069",
+    },
+    {
+      id: 2,
       socialLink: "https://www.linkedin.com/in/akanksharmaa/",
       name: "Akanksha Sharma",
       memojiLogo: akankshaSharma,
       backgroundColour: "#FFE5F8",
     },
     {
-      id: 19,
+      id: 3,
       socialLink: "https://www.linkedin.com/in/akshay-njarangal/",
       name: "Akshay Njarangal",
       memojiLogo: akshayNjarangal,
       backgroundColour: "#f0f0f0",
     },
     {
-      id: 9,
+      id: 4,
       socialLink: "https://www.linkedin.com/in/akshaypakanati/",
       name: "Akshay Pakanati",
       memojiLogo: akshayPakanati,
       backgroundColour: "#A6CCFA",
     },
     {
-      id: 18,
+      id: 5,
+      socialLink: "https://www.linkedin.com/in/amitsamant-dev",
+      name: "Amit Samant",
+      memojiLogo: amitSamant,
+      backgroundColour: "#FFE5F8",
+    },
+    {
+      id: 6,
       socialLink: "https://x.com/nirudhuuu",
       name: "Anirudh Jwala",
       memojiLogo: anirudhJwala,
       backgroundColour: "#e5d5bd",
     },
     {
-      id: 6,
+      id: 7,
+      socialLink: "http://www.linkedin.com/in/ayush21082",
+      name: "Ayush Singh",
+      memojiLogo: ayushSingh,
+      backgroundColour: "#fff3ea",
+    },
+    {
+      id: 8,
+      socialLink: "https://www.linkedin.com/in/bhavin-vaghela",
+      name: "Bhavin Vaghela",
+      memojiLogo: bhavinVaghela,
+      backgroundColour: "#A6CCFA",
+    },
+    {
+      id: 9,
       socialLink: "https://www.linkedin.com/in/bhumika-ios",
       name: "Bhumika Patel",
       memojiLogo: bhumikaPatel,
       backgroundColour: "#FF69E1",
     },
     {
-      id: 13,
+      id: 10,
       socialLink: "https://www.linkedin.com/in/devanshimodha/",
       name: "Devanshi",
       memojiLogo: devanshi,
       backgroundColour: "#FF69E1",
+    },
+    {
+      id: 11,
+      socialLink: "https://www.linkedin.com/in/nikhil-mangrulkar-ph-d-998b1567/",
+      name: "Dr. Nikhil Mangrulkar",
+      memojiLogo: drNikhilMangrulkar,
+      backgroundColour: "#E8D5FF",
     },
     {
       id: 12,
@@ -68,74 +116,130 @@ const MeetTheTeam = () => {
       backgroundColour: "#D6D6D6",
     },
     {
-      id: 5,
+      id: 13,
+      socialLink: "https://www.linkedin.com/in/kajal-sheth",
+      name: "Kajal Sheth",
+      memojiLogo: kajalSheth,
+      backgroundColour: "#FFDEC0",
+    },
+    {
+      id: 14,
+      socialLink: "https://www.linkedin.com/in/krishna-rp/",
+      name: "Krishna Patidar",
+      memojiLogo: krishnaPatidar,
+      backgroundColour: "#C9E4FF",
+    },
+    {
+      id: 15,
+      socialLink: "https://www.linkedin.com/in/krishnapermi/",
+      name: "Krishna Permi",
+      memojiLogo: krishnaPermi,
+      backgroundColour: "#B8E8D0",
+    },
+    {
+      id: 16,
       socialLink: "https://www.linkedin.com/in/manindersinghh",
       name: "Maninder Singh",
       memojiLogo: maninderSingh,
       backgroundColour: "#D6D6D6",
     },
     {
-      id: 4,
+      id: 17,
       socialLink: "https://www.linkedin.com/in/nayyanmujadiya/",
       name: "Nayyan Mujadiya",
       memojiLogo: nayyanMujadiya,
       backgroundColour: "#FFE5F8",
     },
     {
-      id: 20,
+      id: 18,
       socialLink: "https://www.linkedin.com/in/dezinezync/",
       name: "Nikhil Nigade",
       memojiLogo: nikhilNigade,
       backgroundColour: "#f1ae5c",
     },
     {
-      id: 8,
+      id: 19,
       socialLink: "https://www.linkedin.com/in/nishantdesai1/",
       name: "Nishant Desai",
       memojiLogo: nishantDesai,
       backgroundColour: "#FFB069",
     },
     {
-      id: 10,
+      id: 20,
+      socialLink: "https://www.linkedin.com/in/pinak-rokde-42a9a0292/",
+      name: "Pinak Rokde",
+      memojiLogo: pinakRokde,
+      backgroundColour: "#FFD4A8",
+    },
+    {
+      id: 21,
       socialLink: "https://www.linkedin.com/in/sforsethi/",
       name: "Raghav Sethi",
       memojiLogo: raghavSethi,
       backgroundColour: "#FFDEC0",
     },
     {
+      id: 22,
+      socialLink: "https://www.linkedin.com/in/rajhraval/",
+      name: "Raj Raval",
+      memojiLogo: rajRaval,
+      backgroundColour: "#FFDEC0",
+    },
+    {
+      id: 23,
+      socialLink: "https://www.linkedin.com/in/irangareddy/",
+      name: "Ranga Reddy",
+      memojiLogo: rangaReddy,
+      backgroundColour: "#dee6f1",
+    },
+    {
       id: 24,
+      socialLink: "https://www.linkedin.com/in/ratnesh-jain-7a2270146/",
+      name: "Ratnesh Jain",
+      memojiLogo: ratneshJain,
+      backgroundColour: "#D4E7FF",
+    },
+    {
+      id: 25,
       socialLink: "https://www.linkedin.com/in/recursiveswift//",
       name: "Rimesh Jotaniya",
       memojiLogo: rimeshJotaniya,
       backgroundColour: "#f1ae5c",
     },
     {
-      id: 15,
+      id: 26,
+      socialLink: "https://www.linkedin.com/in/rizwan95/",
+      name: "Rizwan Ahmed",
+      memojiLogo: rizwanAhmed,
+      backgroundColour: "#FFC0CB",
+    },
+    {
+      id: 27,
+      socialLink: "https://www.linkedin.com/in/rushit-r-757aa0167/",
+      name: "Rushit Rakhasiya",
+      memojiLogo: rushitRakhasiya,
+      backgroundColour: "#BEFF93",
+    },
+    {
+      id: 28,
       socialLink: "http://linkedin.com/in/shamil-roshan",
       name: "Shamil Roshan",
       memojiLogo: shamilRoshan,
       backgroundColour: "#A6CCFA",
     },
     {
-      id: 21,
-      socialLink: "https://www.linkedin.com/in/recursiveswift//",
+      id: 29,
+      socialLink: "https://www.linkedin.com/in/vedang-kulkarni-vk18/",
       name: "Vedang Kulkarni",
       memojiLogo: vedangKulkarni,
       backgroundColour: "#FFDEC0",
     },
     {
-      id: 22,
+      id: 30,
       socialLink: "https://www.linkedin.com/in/yogeshsingh2810/",
       name: "Yogesh Singh",
       memojiLogo: yogeshSingh,
       backgroundColour: "#BEFF93",
-    },
-    {
-      id: 25,
-      socialLink: "https://www.linkedin.com/in/ratnesh-jain-7a2270146/",
-      name: "Ratnesh Jain",
-      memojiLogo: ratneshJain,
-      backgroundColour: "#D4E7FF",
     },
   ];
 
